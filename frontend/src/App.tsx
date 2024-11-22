f68dc41110afa894e5f1c06a7eb717ce7c517e7d
Lindsay <51120285+loslds@users.noreply.github.com>
2024-11-22 10:02:38 -0300

    aaa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Global from './styles/global';
-
+import { App } from './App';
 import { AcessoProvider } from './components/contexts/ContextAcesso';
-import { AppRoutes } from './components/AppRoutes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Global />
       <AcessoProvider>
-        <AppRoutes />
+        <App />
       </AcessoProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,7 @@
-//import React from 'react';
+import React from 'react';
 
-//import Global from './styles/global';
+import { AppRoutes } from './components/AppRoutes';
 
-import { createRoot } from 'react-dom/client';
-//import { BrowserRouter } from 'react-router-dom';
-
-//import { LoginProvider } from './components/contexts/LoginContext';
-//import { AppRoutes } from './components/AppRoutes';
-
-const root = createRoot(document.querySelector('#root'));
-
-root.render();
-
+const App = () => {
+  return <AppRoutes />;
+};
